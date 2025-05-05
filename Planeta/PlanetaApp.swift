91292ccc3dbdc802d96cd8f37c3acc020a1fc3d7
antonio93hb <antonio93hb@hotmail.com>
2025-05-05 17:46:21 +0200

    Initial Commit

---
+++ Planeta/PlanetaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanetaApp.swift
+//  Planeta
+//
+//  Created by Antonio Hernández Barbadilla on 5/5/25.
+//
+
+import SwiftUI
+
+@main
+struct PlanetaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PlanetaTests/PlanetaTests.swift
@@ -0,0 +1,16 @@
+//
+//  PlanetaTests.swift
+//  PlanetaTests
+//
+//  Created by Antonio Hernández Barbadilla on 5/5/25.
+//
+
+import Testing
+
+struct PlanetaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
